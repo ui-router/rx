a1c534602f0f0c5182be3db5682c61120c3b8b98
Chris Thielen <christopherthielen@users.noreply.github.com>
2016-06-30 08:36:46 -0500

    Create ui-router-react.ts

---
+++ ui-router-react.ts
@@ -0,0 +1,21 @@
+import {Observable, ReplaySubject} from "rxjs/Rx";
+import {Transition, UIRouter, UIRouterGlobals} from "ui-router-ng2";
+
+declare module 'ui-router-ng2/globals' {
+  interface UIRouterGlobals {
+    params$: Observable<{ [paramName: string]: any }>;
+  }
+}
+
+/** Augments UIRouterGlobals with observables for transition starts, successful transitions, and state parameters */
+export class UIRouterRx {
+  constructor(router: UIRouter) {
+
+    let start$ = new ReplaySubject<Transition>();
+    router.transitionService.onStart({}, transition => start$.next(transition));
+    let success$ = <Observable<Transition>> start$.mergeMap((t: Transition) => t.promise.then(() => t));
+    let params$ = success$.map((transition: Transition) => transition.params());
+
+    Object.assign(router.globals, {start$, success$, params$});
+  }
+}
